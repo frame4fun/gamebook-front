49a77b0295474e07f71b1bc3d16b0beb7a642747
aliba <19654755+alice-ba@users.noreply.github.com>
2019-05-08 23:02:46 +0200

    .

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.css';
+console.log(styles);
+class Header extends Component {
+  render() {
+    return (
+      <div className={styles.header}>
+        <div className={styles['header-left']}>
+          <NavLink className={styles['header-text']} to="/">
+            Home
+          </NavLink>
+          <NavLink className={styles['header-text']} to="/profile/">
+            Profile
+          </NavLink>
+        </div>
+        <div className={styles['header-right']}>
+          <NavLink className={styles['header-text']} to="/signin/">
+            Sign In
+          </NavLink>
+          <NavLink className={styles['header-text']} to="/signup/">
+            Sign Up
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+}
 
-const head = () => (
-  <div className={styles.div}>
-    <NavLink to="/signin/">Sign In</NavLink>
-    <NavLink to="/signup/">Sign Up</NavLink>
-  </div>
-);
-
-export default head;
+export default Header;
