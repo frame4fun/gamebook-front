136b6c1c8150e211b14b9b2773f3002e9134099c
aliba <19654755+alice-ba@users.noreply.github.com>
2019-05-25 12:18:10 +0200

    profile creation and connexion improved

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import { SignInForm } from '../Connexion/SignInForm';
 import { SignUpForm } from '../Connexion/SignUpForm';
+import { Profile } from '../Pages/Profile';
 import { Welcome } from '../Pages/Welcome';
 
 class Routing extends Component {
   handleCancel = () => this.props.history.push('/');
+
   render() {
     return (
       <div>
         <Route
           path="/signin/"
           render={props => (
-            <SignInForm {...props} handleCancel={this.handleCancel} />
+            <SignInForm
+              {...props}
+              handleCancel={this.handleCancel}
+              handleChange={this.handleChange}
+            />
           )}
         />
         <Route
           path="/signup/"
           render={props => (
-            <SignUpForm {...props} handleCancel={this.handleCancel} />
+            <SignUpForm
+              {...props}
+              handleCancel={this.handleCancel}
+              handleChange={this.handleChange}
+            />
+          )}
+        />
+        <Route
+          path="/profile/"
+          render={props => (
+            <Profile {...props} handleCancel={this.handleCancel} />
           )}
         />
         <Route path="/" component={Welcome} />
